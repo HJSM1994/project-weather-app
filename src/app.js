b1b274b64d432bafab9be30979cd98da3d0b8534
HJSM1994 <147425155+HJSM1994@users.noreply.github.com>
2023-10-11 15:23:38 +0100

    Removed console.log for testing

--- src/app.js
+++ src/app.js
@@ -38,7 +38,5 @@ function displayTemperature(response) {
 
 let apiKey = "b95f179627c8dd37f41e1be6e3250e19";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Manchester,UK&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
