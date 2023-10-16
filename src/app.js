9df04063ba131d936d4b3e5f5f0adc9b5b4385e0
HJSM1994 <147425155+HJSM1994@users.noreply.github.com>
2023-10-16 15:09:50 +0100

    HTML removed and implemented into JS
    Future forecast HTML removed and implemented into JS - new function added to create  concatenation / loop to replicate future forecast - API will be added at a later date

--- src/app.js
+++ src/app.js
@@ -21,6 +21,35 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class = "row">`;
+  let days = ["Thursday", "Friday", "Saturday", "Sunday"];
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+        <div class="col-2">
+          <div class="weather-forecast-date">${day}</div>
+          <img
+            src="https://openweathermap.org/img/wn/01d@2x.png"
+            width="42px"
+          />
+          <br />
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max">18°</span>
+            <span class="weather-forecast-temperature-min">12°</span>
+          </div>
+        </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#currentTemp");
   let cityElement = document.querySelector("#city");
@@ -88,3 +117,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
