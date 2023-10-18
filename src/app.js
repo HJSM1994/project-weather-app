f7442f03200a23ef626794f3eab18b61a1cf5213
HJSM1994 <147425155+HJSM1994@users.noreply.github.com>
2023-10-18 14:58:10 +0100

    CSS / view updates
    Positioning and background colour updates

--- src/app.js
+++ src/app.js
@@ -67,7 +67,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "b95f179627c8dd37f41e1be6e3250e19";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
@@ -141,4 +140,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("New York");
+search("Manchester");
